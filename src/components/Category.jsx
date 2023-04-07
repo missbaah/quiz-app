bcbd0ff370b8e60b0144cf7f2d8c136217b03fe5
Adwoa Addo-Brako <baahbrako@gmail.com>
2023-04-07 11:38:15 +0000

    fetching quiz data from API in Category.jsx

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,7 +1,52 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import he from "he";
 
 const Category = () => {
-  return <div>Category</div>;
+  const location = useLocation();
+  const categoryName = new URLSearchParams(location.search).get("categoryName");
+  const [questions, setQuestions] = useState([]);
+
+  let categoryNum = 0;
+
+  if (categoryName === "Math") {
+    categoryNum = 19;
+  } else if (categoryName === "Science") {
+    categoryNum = 17;
+  } else if (categoryName === "Geography") {
+    categoryNum = 22;
+  } else if (categoryName === "History") {
+    categoryNum = 23;
+  }
+
+  const amount = 5; // set the number of questions you want to fetch
+  const difficulty = "easy"; // set the difficulty level of the questions
+
+  useEffect(() => {
+    fetch(
+      `https://opentdb.com/api.php?amount=5&category=${categoryNum}&difficulty=easy&type=multiple`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setQuestions([...data.results]);
+      })
+      .catch((error) => {
+        console.log("Error:" + error);
+      });
+  }, []);
+
+  console.log(questions);
+
+  const ListOfQuestions = questions.map((qtn) => {
+    return <p key={qtn.question}>{he.decode(qtn.question)}</p>;
+  });
+
+  return (
+    <div>
+      <h3>{categoryName}</h3>
+      <div>{ListOfQuestions}</div>
+    </div>
+  );
 };
 
 export default Category;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,45 @@
 import React from "react";
+import Category from "./Category";
 
 const Home = () => {
+  //   const category = ["Math", "Science", "Geography"];
+
+  const handleClick = (categoryName) => {
+    window.location.href = `/category?categoryName=${categoryName}`;
+  };
+
   return (
     <main>
       <h2>Choose your category</h2>
       <section className="btns">
-        <button>Math</button>
-        <button>Science</button>
-        <button>Geography</button>
+        <button
+          onClick={() => {
+            handleClick("Math");
+          }}
+        >
+          Math
+        </button>
+        <button
+          onClick={() => {
+            handleClick("Science");
+          }}
+        >
+          Science
+        </button>
+        <button
+          onClick={() => {
+            handleClick("Geography");
+          }}
+        >
+          Geography
+        </button>
+        <button
+          onClick={() => {
+            handleClick("History");
+          }}
+        >
+          Geography
+        </button>
       </section>
     </main>
   );
